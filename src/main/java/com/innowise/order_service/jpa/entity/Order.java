e79d6d8dfd90bd8e72687d45aab185293f9d71a1
Howksick <yanzakharevsk@mail.ru>
2026-06-10 16:59:02 +0300

    Entity Order has been written

---
+++ src/main/java/com/innowise/order_service/jpa/entity/Order.java
@@ -0,0 +1,69 @@
+package com.innowise.order_service.jpa.entity;
+
+import com.innowise.order_service.jpa.enums.OrderStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order extends BaseAuditableEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, updatable = false)
+    private Long userId;
+
+    @OneToMany(mappedBy = "order", orphanRemoval = true, cascade = CascadeType.ALL)
+    @ToString.Exclude
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status;
+
+    @Column(name = "total_price", nullable = false, precision = 10, scale = 2)
+    private BigDecimal totalPrice;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
+
+    @Override
+    public final boolean equals(Object o) {
+        if(this == o) return true;
+        if(o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy
+                ?((HibernateProxy) o).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy
+                ?((HibernateProxy) this).getHibernateLazyInitializer()
+                 .getPersistentClass()
+                : this.getClass();
+        if(thisEffectiveClass != oEffectiveClass) return false;
+        Order order = (Order) o;
+        return getId() != null && Objects.equals(getId(), order.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return this instanceof HibernateProxy
+                ? ((HibernateProxy) this).getHibernateLazyInitializer()
+                  .getPersistentClass()
+                  .hashCode()
+                : getClass().hashCode();
+    }
+}
